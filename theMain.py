c4c14e825b0c13c0e80faf47b89f38264df11e84
root <root@localhost.localdomain>
2019-06-14 00:46:23 +0800

    Fix wx token verify, the sample is not suitable for python3.

--- theMain.py
+++ theMain.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import hashlib
 
 app = Flask(__name__)
 
@@ -6,13 +7,37 @@ app = Flask(__name__)
 # for test the token from weixin
 @app.route('/wx')
 def hello_world():
-        print(request)
-        data = request.args
-        print(data)
-        #print(data['token'])
+	print(request)
+	data = request.args
+	print(data)
+	#print(data['token'])
+	#pri = web.input()
+	if len(data) == 0 :
+		return "wrong things"
 
-        #print(request.name)
-        return 'Hello, World!'
+	signature = data['signature']
 
-#if __name__ == "__main__":
-#    app.run()
+	timestamp = data['timestamp']
+	nonce = data['nonce']
+	echostr = data['echostr']
+
+	print("args is ", signature, timestamp, nonce, echostr)
+
+	token = "123"
+	list = [token, timestamp,nonce]
+	list.sort()
+	listStr = ''.join(list)
+
+	sha1 = hashlib.sha1()
+	#map(sha1.update, list)
+	#hashcode = sha1.hexdigest()
+	sha1.update(listStr.encode('utf-8'))
+	hashcode = sha1.hexdigest()
+	
+	print ( "info is ", hashcode, signature)
+	print("timestamp is" , timestamp)
+
+	if hashcode == signature:
+		return echostr
+	else:
+		return ""
